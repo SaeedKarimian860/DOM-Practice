81795f0d340da79620fe7642a9fc5a0963d0186f
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-13 21:58:32 -0500

    DOM Practice

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ for (let link of allLinks) {
   link.style.textDecorationColor = "magenta";
   link.style.textDecorationStyle = "wavy";
 }
+
+
+// const allImages = document.getElementsByTagName('img');
+
+// for (let img of allImages) {
+//   img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg"
+// }
+//this changed all images to all be the same one.
+
+
+
+const squareImages = document.getElementsByClassName('square');
+
+for (let img of squareImages) {
+  img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg"
+}
