8d2da6e1c36b2129204d34ee719f26a51d311306
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-18 22:03:18 -0500

    Event Basics

--- EventBasics/app.js
+++ EventBasics/app.js
@@ -14,3 +14,9 @@ btn.onmouseenter = scream;
 document.querySelector('h1').onclick = () => {
     alert('you clicked the h1!!');
 }
+
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', () => {
+    alert('clicked!');
+})
