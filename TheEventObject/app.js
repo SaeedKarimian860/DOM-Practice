417bb406c646e6b81132670d0605d5dcd64cfd84
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-19 23:46:53 -0500

    Events Object

--- TheEventObject/app.js
+++ TheEventObject/app.js
@@ -10,3 +10,22 @@ document.querySelector('button').addEventListener('click', (evt) => {
 // input.addEventListener('keyup', () => {
 //     console.log('KEYUP');
 // })
+
+window.addEventListener('keydown', (e) => {
+    switch(e.code) {
+        case 'ArrowUp':
+            console.log('up!');
+            break;
+        case 'ArrowDown':
+            console.log('down!');
+            break;
+        case 'ArrowRight':
+            console.log('right!');
+            break;
+        case 'ArrowLeft':
+            console.log('left!');
+            break;
+        default:
+            console.log('Ignored!')
+    }
+})
