d13412bd00442530b23c5c0dc471af5614fb4151
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-19 23:39:55 -0500

    Events Object

--- TheEventObject/app.js
+++ TheEventObject/app.js
@@ -2,10 +2,11 @@ document.querySelector('button').addEventListener('click', (evt) => {
     console.log(evt);
 });
 
-const input = document.querySelector('input');
-input.addEventListener('keydown', (e) => {
-    console.log(e);
-})
+// const input = document.querySelector('input');
+// input.addEventListener('keydown', (e) => {
+//     console.log(e.key);
+//     console.log(e.code);
+// })
 // input.addEventListener('keyup', () => {
 //     console.log('KEYUP');
 // })
